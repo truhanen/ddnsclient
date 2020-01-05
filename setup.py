98a1ff34bea9f95554dceec29558fc791c089545
Tuukka Ruhanen <tuukka.ruhanen@firstbeat.com>
2020-01-05 18:02:48 +0200

    Add missing requirement to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1',
     description=('Simple service for updating dynamic IP\'s of a host '
                  'to the dynamic DNS service of namecheap.com'),
-    install_requires=['requests', 'bs4'],
+    install_requires=['requests', 'bs4', 'lxml'],
     python_requires='>=3.6',
     py_modules=['dyndns_service'],
     scripts=['dyndns_service.py'],
