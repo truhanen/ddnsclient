7081327c96bdd3aa62ad2dbb1f10688b5dff28fb
Tuukka Ruhanen <tuukka.ruhanen@firstbeat.com>
2020-04-08 20:03:13 +0300

    Rename the package as dynamic_dns_updater

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup
 
 setup(
-    name='dyndns_service',
-    version='0.1',
+    name='dynamic_dns_updater',
+    version='0.1.0',
     description=('Simple service for updating dynamic IP\'s of a host '
                  'to the dynamic DNS service of namecheap.com'),
     install_requires=['requests', 'bs4', 'lxml'],
     python_requires='>=3.6',
-    py_modules=['dyndns_service'],
-    scripts=['dyndns_service.py'],
+    py_modules=['dynamic_dns_updater'],
+    scripts=['dynamic_dns_updater.py'],
 )
